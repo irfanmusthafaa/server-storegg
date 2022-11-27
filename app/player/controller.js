2b72e7fee1053a85d0a2fc260615f6b80a5ced3d
irfanmusthafaa <irfanmustafa2307@gmail.com>
2022-11-27 14:42:57 +0700

    Update API detail Page

--- app/player/controller.js
+++ app/player/controller.js
@@ -33,11 +33,19 @@ module.exports = {
             .populate('nominals')
             .populate('user', '_id name phoneNumber')
 
+            const payment = await Payment.find().populate('banks')
+
             if(!voucher){
                 return  res.status(404).json({ message: "Data voucher tidak ditemukan !"})
             }
 
-            res.status(200).json({ data: voucher})
+            res.status(200).json({ 
+                data: {
+                    detail: voucher,
+                    payment
+                }
+                
+            })
         } catch (err) {
             res.status(500).json({ message: err.message || `Internal server error`})
         }
